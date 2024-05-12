0b31f161d4c50bda8f1566221d7af42e7a6a7389
Sushantshrestha1111 <shresthasushant11@gmail.com>
2024-05-12 21:26:19 +0545

    db connection

---
+++ initializers/loadEnvVariables.go
@@ -0,0 +1,19 @@
+package initializers
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		if err != nil {
+			log.Fatal("Error loading.env file")
+		}
+	}
+
+}
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-auth/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+
+}
 
 func main() {
 
 	fmt.Println("hello2")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 
 }
