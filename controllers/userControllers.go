e3bd2dc6854d5ef77dd10a8ac52d9ec3bc09fafd
Sushantshrestha1111 <shresthasushant11@gmail.com>
2024-05-15 06:21:37 +0545

    cookie

--- controllers/userControllers.go
+++ controllers/userControllers.go
@@ -113,8 +113,8 @@ func Login(c *gin.Context) {
 	fmt.Println(tokenString, err)
 
 	//send it back
-	c.JSON(http.StatusOK, gin.H{
-		"token": tokenString,
-	})
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+	c.JSON(http.StatusOK, gin.H{})
 
 }
